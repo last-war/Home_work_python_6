65ded15de43b296ea3d5e4185e8c70a0a2f77226
last-war <andrey.maybrodskyy@gmail.com>
2022-10-22 13:30:16 +0300

    test ver

--- test.py
+++
@@ -1,9 +0,0 @@
-import my_sort
-from pathlib import Path
-
-
-my_sort.sort_dir(Path("C:\\MyPyton\\python-course\\test1"))
-
-my_sort.sort_dir(Path("C:\\MyPyton\\python-course\\.test1"))
-
-my_sort.sort_dir(Path("C:\\MyPyton\\python-course\\test.2"))
